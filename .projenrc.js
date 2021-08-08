9ce4a70c88076fe6429f4309b42f31c514ce7320
Sebastian Schlecht <mail@sebastian-schlecht.de>
2021-08-08 10:54:24 +0200

    Add project type

--- .projenrc.js
+++ .projenrc.js
@@ -1,26 +1,34 @@
-const { AwsCdkConstructLibrary, NodePackageManager } = require('projen');
+const {
+  AwsCdkConstructLibrary,
+  NodePackageManager,
+  ProjectType,
+} = require("projen");
 const project = new AwsCdkConstructLibrary({
-  author: 'Sebastian Schlecht',
-  authorAddress: 'mail@sebastian-schlecht.de',
-  cdkVersion: '1.95.2',
-  defaultReleaseBranch: 'main',
-  name: 'cdk-stack-ttl',
+  author: "Sebastian Schlecht",
+  authorAddress: "mail@sebastian-schlecht.de",
+  cdkVersion: "1.95.2",
+  defaultReleaseBranch: "main",
+  name: "cdk-stack-ttl",
   packageManager: NodePackageManager.NPM,
-  repositoryUrl: 'git@github.com:sebastian-schlecht/cdk-stack-ttl.git',
+  repositoryUrl: "git@github.com:sebastian-schlecht/cdk-stack-ttl.git",
 
   cdkDependencies: [
-    '@aws-cdk/core',
-    '@aws-cdk/aws-lambda',
-    '@aws-cdk/aws-events',
-    '@aws-cdk/aws-events-targets',
-    '@aws-cdk/aws-iam',
+    "@aws-cdk/core",
+    "@aws-cdk/aws-lambda",
+    "@aws-cdk/aws-events",
+    "@aws-cdk/aws-events-targets",
+    "@aws-cdk/aws-iam",
   ] /* Which AWS CDK modules (those that start with "@aws-cdk/") does this library require when consumed? */,
-  // cdkTestDependencies: undefined,    /* AWS CDK modules required for testing. */
-  deps: ['moment'] /* Runtime dependencies of this module. */,
+  cdkTestDependencies: [
+    "@aws-cdk/assert",
+  ] /* AWS CDK modules required for testing. */,
+  deps: ["moment"] /* Runtime dependencies of this module. */,
+  peerDeps: ["moment"],
   // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],                       /* Build dependencies for this module. */
   // packageName: undefined,            /* The "name" in package.json. */
-  // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
+  projectType:
+    ProjectType.AwsCdkConstructLibrary /* Which type of project this is (library/app). */,
   // release: undefined,                /* Add release management to this project. */
 });
 project.synth();
