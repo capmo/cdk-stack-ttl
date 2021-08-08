0c4b7e966278b75d9d88d595bf68d8511a63a8a1
Sebastian Schlecht <mail@sebastian-schlecht.de>
2021-08-08 11:18:55 +0200

    Refactor tests to vanilla JS

--- .projenrc.js
+++ .projenrc.js
@@ -22,8 +22,7 @@ const project = new AwsCdkConstructLibrary({
   cdkTestDependencies: [
     '@aws-cdk/assert',
   ] /* AWS CDK modules required for testing. */,
-  deps: ['moment'] /* Runtime dependencies of this module. */,
-  peerDeps: ['moment'],
+  // deps: [] /* Runtime dependencies of this module. */,
   // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],                       /* Build dependencies for this module. */
   // packageName: undefined,            /* The "name" in package.json. */
--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,10 @@
-import * as moment from 'moment';
-
 // See https://docs.aws.amazon.com/lambda/latest/dg/tutorial-scheduled-events-schedule-expressions.html
 export const cronExpression = (startDate: Date, ttl: number) => {
-  const deletionTime = moment.utc(startDate).add(ttl, 'm');
+  const deletionTime = new Date(startDate.getTime() + ttl * 1000 * 60);
 
-  const cron = `cron(${deletionTime.minute()} ${deletionTime.hour()} ${deletionTime.date()} ${
-    deletionTime.month() + 1
-  } ? ${deletionTime.year()})`;
+  const cron = `cron(${deletionTime.getUTCMinutes()} ${deletionTime.getUTCHours()} ${deletionTime.getUTCDate()} ${
+    deletionTime.getUTCMonth() + 1
+  } ? ${deletionTime.getUTCFullYear()})`;
 
   return cron;
 };
