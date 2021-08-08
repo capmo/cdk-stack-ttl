0cbb226997e9b88c0fe9b36d2bf9f9f9bb611e6b
Sebastian Schlecht <mail@sebastian-schlecht.de>
2021-08-08 11:10:59 +0200

    Add util test

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,6 +1,9 @@
 import { cronExpression } from '../src/utils';
 
 test('utils', () => {
-  const today = new Date('2021-08-08T08:47:01.876Z');
-  expect(cronExpression(today, 3600)).toBe('cron(47 20 10 8 ? 2021)');
+  const ts1 = new Date('2021-08-08T08:47:01.876Z');
+  expect(cronExpression(ts1, 3600)).toBe('cron(47 20 10 8 ? 2021)');
+
+  const ts2 = new Date('2021-08-06T09:57:01.876Z');
+  expect(cronExpression(ts2, 60)).toBe('cron(57 10 6 8 ? 2021)');
 });
