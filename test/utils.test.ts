3fa1bc7a0e16ae20dfac2e57f6d0d5e700357e35
K Mang <klemens.mang@me.com>
2021-08-30 23:03:30 +0200

    [SI-53] Change stack ttl calculation to days (#10)
    * Bump cdk dependencies to fix version mismatch with importing repos
    
    * Change stack TTL calculation to days instead of minutes
    
    * Fix tests
    
    * CF Export TTL cron schedule string
    
    * Added some comments
    
    * chore: self mutation
    
    Co-authored-by: Klemens Mang <klemens.mang@capmo.de>
    Co-authored-by: Automation <github-actions@github.com>

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -2,8 +2,8 @@ import { cronExpression } from '../src/utils';
 
 test('utils', () => {
   const ts1 = new Date('2021-08-08T08:47:01.876Z');
-  expect(cronExpression(ts1, 3600)).toBe('cron(47 20 10 8 ? 2021)');
+  expect(cronExpression(ts1, 1)).toBe('cron(47 8 9 8 ? 2021)');
 
   const ts2 = new Date('2021-08-06T09:57:01.876Z');
-  expect(cronExpression(ts2, 60)).toBe('cron(57 10 6 8 ? 2021)');
+  expect(cronExpression(ts2, 30)).toBe('cron(57 9 5 9 ? 2021)');
 });
